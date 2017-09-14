1f959ce17217240cd90c8d9e8f95ba1d1e82ccd3
nikitav07 <31950438+nikitav07@users.noreply.github.com>
2017-09-14 13:31:04 +0530

    Add files via upload

---
+++ nikita.py
@@ -0,0 +1,9 @@
+print("program to identify if the number is even or odd")
+
+num=int(input("enter a number"))
+
+if(num%2)==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+    
